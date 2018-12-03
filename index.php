418cb1d13a3fccff38ab0dfd1f6756b3d452c5df
Killian BALLAND <killian.balland@outlook.fr>
2018-12-03 21:33:54 +0100

    Mise à jour

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ include_once "vendor/autoload.php";
 use Illuminate\Database\Capsule\Manager;
 use MyWishList\modele\Liste;
 use MyWishList\modele\Item;
+use \Slim\Http\Response;
 use Slim\App;
 
 $db = new Manager();
@@ -13,20 +14,23 @@ $db->setAsGlobal();
 $db->bootEloquent();
 
 $app = new App();
-$app->get('/listes', function ($request, $response, $args) {
-    $listes = Liste::get();
-    $content = "";
-    foreach($listes as $liste) {
-        $content .= $liste . '<br />';
-    }
-    $response->write($content);
-});
 
 $app->get('/liste/{no}', function ($request, $response, $args) {
     $liste = Liste::where('no', '=', $args['no'])->first();
     if(isset($liste)) {
         $response->write($liste . '<br />');
     }
+})->setName('liste');
+
+$app->get('/listes', function ($request, $response) {
+    global $app;
+    $listes = Liste::get();
+    $content = "";
+    foreach($listes as $liste) {
+        $href = $app->getContainer()->get('router')->pathFor('liste', ['no' => $liste->no]);
+        $content .= "<a href='$href'>Liste $liste->no</a><br />";
+    }
+    $response->write($content);
 });
 
 $app->get('/item/{id}', function ($request, $response, $args) {
@@ -35,5 +39,12 @@ $app->get('/item/{id}', function ($request, $response, $args) {
         $response->write($item . '<br />');
     }
 });
+
+$app->getContainer()['notFoundHandler'] = function () {
+    return function() {
+        $response = new Response(404);
+        return $response->write('test');
+    };
+};
 
 $app->run();
